9ce4fcb31a20a264ba4ce9d7616141fc82824b47
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-06-14 14:48:46 +0200

    Added HOMEWORK#2

---
+++ HOMEWORK#2/SEDC.PizzaApp/PizzaApp/Models/Mappers/PizzaMapperHelpers.cs
@@ -0,0 +1,25 @@
+﻿using PizzaApp.Models.Domain;
+using PizzaApp.Models.ViewModels.PizzaViewModels;
+
+internal static class PizzaMapperHelpers
+{
+
+    public static PizzaViewModel MapFromPizzaToPizzaViewModel(this Pizza pizza)
+    {
+        if (pizza.HasExtras)
+        {
+            pizza.Price += 10;
+        }
+
+        return new PizzaViewModel()
+        {
+            Id = pizza.Id,
+            Name = pizza.Name,
+            Price = pizza.Price,
+            PizzaSize = pizza.PizzaSize,
+            HasExtras = pizza.HasExtras
+        };
+
+
+    }
+}
---
+++ HOMEWORK#2/SEDC.PizzaApp/PizzaApp/Models/ViewModels/PizzaViewModels/PizzaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PizzaApp.Models.Enums;
+
+namespace PizzaApp.Models.ViewModels.PizzaViewModels
+{
+    public class PizzaViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public PizzaSize PizzaSize { get; set; }
+        public bool HasExtras { get; set; }
+    }
+}
